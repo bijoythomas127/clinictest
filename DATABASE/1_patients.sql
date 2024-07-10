a80691759bb78b9b93cc82c6182cea726d9f9367
Bijoy Thomas <bijoythomas127@gmail.com>
2024-07-10 22:40:33 +0530

    message

--- DATABASE/1_patients.sql
+++ DATABASE/1_patients.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `patients` (
     `first_name` VARCHAR(50) NOT NULL,
     `surname` VARCHAR(50) NOT NULL,
     `dob` DATE NOT NULL,
-    `created_at` TIMESTAMP DEFAULT CUURENT_TIMESTAMP, 
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
